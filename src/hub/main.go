e636509c51fe88df3118880779758d839fb01124
Christoph Baier <chrisby@crispy-coding.org>
2024-06-14 12:23:00 +0200

    adding logging

--- src/hub/main.go
+++ src/hub/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/ocelot-cloud/shared"
 	"io"
 	"log"
 	"net/http"
@@ -20,6 +21,8 @@ import (
 
 const uploadPath = "./users" // TODO Create folder if not exist
 
+var Logger = shared.ProvideLogger()
+
 func main() {
 	err := os.MkdirAll(uploadPath, os.ModePerm) // TODO Make permissions 600
 	if err != nil {
@@ -29,19 +32,19 @@ func main() {
 	http.HandleFunc("/upload", uploadHandler)
 	http.HandleFunc("/download/", downloadHandler)
 
-	log.Println("Server started on :8082")
+	Logger.Info("Server started on :8082")
 	log.Fatal(http.ListenAndServe(":8082", nil))
 }
 
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
-		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		logAndRespondError(w, "Invalid request method", http.StatusMethodNotAllowed)
 		return
 	}
 
 	file, header, err := r.FormFile("file")
 	if err != nil {
-		http.Error(w, "Failed to get file from request", http.StatusBadRequest)
+		logAndRespondError(w, "Failed to get file from request", http.StatusBadRequest)
 		return
 	}
 	defer file.Close()
@@ -49,39 +52,42 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	// TODO Add test
 	// TODO Make security test that user and repo are in the name correctly, and that both exist.
 	if !strings.HasSuffix(header.Filename, ".tar.gz") {
-		http.Error(w, "Invalid file type", http.StatusBadRequest)
+		logAndRespondError(w, "Invalid file type", http.StatusBadRequest)
 		return
 	}
 
 	out, err := os.Create(filepath.Join(uploadPath, header.Filename))
 	if err != nil {
-		log.Printf("Failed to save file: %v\n", err.Error())
-		http.Error(w, "Failed to save file", http.StatusInternalServerError)
+		logAndRespondError(w, "Failed to save file", http.StatusInternalServerError)
 		return
 	}
 	defer out.Close()
 
 	_, err = io.Copy(out, file)
 	if err != nil {
-		log.Printf("Failed to save file: %v\n", err.Error())
-		http.Error(w, "Failed to save file", http.StatusInternalServerError)
+		logAndRespondError(w, "Failed to save file", http.StatusInternalServerError)
 		return
 	}
 
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintln(w, "File uploaded successfully")
+	fmt.Fprintln(w, "File uploaded successfully") // TODO to be logged
+}
+
+func logAndRespondError(w http.ResponseWriter, msg string, httpStatus int) {
+	Logger.Error(msg)
+	http.Error(w, msg, http.StatusMethodNotAllowed)
 }
 
 func downloadHandler(w http.ResponseWriter, r *http.Request) {
 	fileName := strings.TrimPrefix(r.URL.Path, "/download/")
 	if fileName == "" {
-		http.Error(w, "File name is missing", http.StatusBadRequest)
+		logAndRespondError(w, "File name is missing", http.StatusBadRequest)
 		return
 	}
 
 	filePath := filepath.Join(uploadPath, fileName)
 	if _, err := os.Stat(filePath); os.IsNotExist(err) {
-		http.Error(w, "File not found", http.StatusNotFound)
+		logAndRespondError(w, "File not found", http.StatusNotFound)
 		return
 	}
 
