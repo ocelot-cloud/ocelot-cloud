8d77a3905341c41765bd5be674f37938a5ef8654
Christoph Baier <chrisby@crispy-coding.org>
2024-07-08 14:04:58 +0200

    added input validation

--- src/hub/input_validator.go
+++ src/hub/input_validator.go
@@ -1,18 +1,54 @@
 package main
 
-// For usernames, apps
+import (
+	"net/url"
+	"regexp"
+	"strconv"
+)
+
 func validateName(input string) bool {
-	return false
+	pattern := `^[a-z0-9]{3,20}$`
+	re := regexp.MustCompile(pattern)
+	return re.MatchString(input)
 }
 
 func validateTag(input string) bool {
-	return false
+	pattern := `^[a-z0-9.]{3,20}$`
+	re := regexp.MustCompile(pattern)
+	return re.MatchString(input)
 }
 
 func validatePasswords(input string) bool {
-	return false
+	pattern := `^[a-z0-9!@#\$%\^&\*\(\)_\+\-=\[\]\{\};':",.<>\/?\\|` + "`" + `~]{3,20}$`
+	re := regexp.MustCompile(pattern)
+	return re.MatchString(input)
 }
 
 func validateOrigin(input string) bool {
-	return false
+	parsedURL, err := url.Parse(input)
+	if err != nil {
+		return false
+	}
+
+	if parsedURL.Scheme != "http" && parsedURL.Scheme != "https" {
+		return false
+	}
+
+	host := parsedURL.Hostname()
+	if len(host) == 0 {
+		return false
+	}
+
+	if parsedURL.Port() != "" {
+		port, err := strconv.Atoi(parsedURL.Port())
+		if err != nil || port < 1 || port > 65535 {
+			return false
+		}
+	}
+
+	if parsedURL.Path != "" || parsedURL.RawQuery != "" || parsedURL.Fragment != "" {
+		return false
+	}
+
+	return true
 }
---
+++ src/hub/input_validator_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"github.com/ocelot-cloud/shared/assert"
+	"testing"
+)
+
+func TestValidateName(t *testing.T) {
+	assert.True(t, validateName("validusername"))
+	assert.True(t, validateName("user123"))
+	assert.False(t, validateName("InvalidUsername"))          // Contains uppercase
+	assert.False(t, validateName("user!@#"))                  // Contains special characters
+	assert.False(t, validateName("us"))                       // Too short
+	assert.False(t, validateName("thisusernameiswaytoolong")) // Too long
+}
+
+func TestValidateTag(t *testing.T) {
+	assert.True(t, validateTag("valid.tagname"))
+	assert.True(t, validateTag("tag123"))
+	assert.True(t, validateTag("tag.name123"))
+	assert.False(t, validateTag("invalid.tagname!"))             // Contains special characters other than dot
+	assert.False(t, validateTag("ta"))                           // Too short
+	assert.False(t, validateTag("this.tagname.is.way.too.long")) // Too long
+}
+
+func TestValidatePasswords(t *testing.T) {
+	assert.True(t, validatePasswords("validpassword!"))
+	assert.True(t, validatePasswords("valid_pass123"))
+	assert.False(t, validatePasswords("InvalidPassword"))           // Contains uppercase
+	assert.True(t, validatePasswords("valid!@#"))                   // Contains special characters
+	assert.False(t, validatePasswords("vp"))                        // Too short
+	assert.False(t, validatePasswords("thispasswordiswaytoolong!")) // Too long
+}
+
+func TestValidateOrigin(t *testing.T) {
+	assert.True(t, validateOrigin("http://example.com"))
+	assert.True(t, validateOrigin("https://example.com"))
+	assert.True(t, validateOrigin("http://example.com:8080"))
+	assert.False(t, validateOrigin("http://example.com/path"))      // Contains path
+	assert.False(t, validateOrigin("ftp://example.com"))            // Invalid scheme
+	assert.False(t, validateOrigin("https://example.com:99999"))    // Invalid port
+	assert.False(t, validateOrigin("https://example.com?query=1"))  // Contains query
+	assert.False(t, validateOrigin("https://example.com#fragment")) // Contains fragment
+	assert.False(t, validateOrigin("http://"))                      // Missing hostname
+	assert.False(t, validateOrigin("http://:8080"))                 // Missing hostname
+}
