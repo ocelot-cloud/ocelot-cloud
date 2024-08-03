673091d2a4fc3354482cccb7301f6f277c6f0e9c
Christoph Baier <chrisby@crispy-coding.org>
2024-08-03 13:18:52 +0200

    refactoring

--- src/cloud/frontend/src/components/Hub.vue
+++ src/cloud/frontend/src/components/Hub.vue
@@ -1,22 +1,46 @@
+<!-- Add this new section inside your existing template -->
 <template>
   <div id="app" class="container mt-5">
     <h3>Ocelot Hub</h3>
     <div class="d-flex justify-content-end align-items-center mb-3">
       <span class="me-2">Logged in as: {{ user }}</span>
       <button class="btn btn-primary" @click="logout">Logout</button>
+      <button class="btn btn-danger" @click="showDeleteConfirmation = true">Delete Account</button>
     </div>
     <router-view />
+
+    <!-- Delete Confirmation Modal -->
+    <div v-if="showDeleteConfirmation" class="modal fade show" style="display: block;" tabindex="-1">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">Confirm Account Deletion</h5>
+            <button type="button" class="btn-close" @click="showDeleteConfirmation = false" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <p>Are you sure you want to delete your account?</p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" @click="showDeleteConfirmation = false">Cancel</button>
+            <button type="button" class="btn btn-danger" @click="confirmDeleteAccount">Confirm</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
 
 <script lang="ts">
 import { defineComponent, ref, onMounted } from 'vue';
 import axios from "axios";
 import router from "@/router";
+
 export default defineComponent({
   name: 'HubComponent',
   setup() {
     const user = ref<string | null>(null);
+    const showDeleteConfirmation = ref(false);
 
     const checkAuth = async () => {
       const url = 'http://localhost:8082';
@@ -43,12 +67,17 @@ export default defineComponent({
     const deleteAccount = async () => {
       try {
         const url = 'http://localhost:8082';
-        await axios.get(url + "/delete");
+        await axios.delete(url + "/user");
         user.value = "";
         redirectToLogin();
       } catch (error) {
         alert(error);
       }
+    };
+
+    const confirmDeleteAccount = async () => {
+      showDeleteConfirmation.value = false;
+      await deleteAccount();
     };
 
     const redirectToLogin = () => {
@@ -62,10 +91,14 @@ export default defineComponent({
     return {
       user,
       logout,
+      showDeleteConfirmation,
+      deleteAccount,
+      confirmDeleteAccount,
     };
   },
 });
 </script>
+
 
 TODO: Hub
 * DeleteUser
