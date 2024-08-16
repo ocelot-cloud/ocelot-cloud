92df973ceaf7a671f62dd11d12be89df1a62877c
Christoph Baier <chrisby@crispy-coding.org>
2024-08-16 08:41:38 +0200

    refactoring

---
+++ src/cloud/frontend/src/components/hub/HubAppManagement.vue
@@ -0,0 +1,120 @@
+<template>
+  <div v-if="!isEditingTags">
+    <h4 >App Management</h4>
+    <div class="d-flex justify-content-center mb-3">
+      <div class="col-6">
+        <input id="input-app" v-model="newAppToCreate" class="form-control" placeholder="Name of New App" required />
+      </div>
+    </div>
+    <button id="button-create-app" @click="createApp" class="btn btn-primary">Create App</button>
+    <br>
+    <br>
+    <h4>App List</h4>
+    <p v-if="appList == null"> (no apps created yet) </p>
+    <div class="d-flex justify-content-center">
+      <ul id="app-list" class="list-group">
+        <li
+            v-for="(app, index) in appList"
+            :key="app"
+            class="list-group-item"
+            :class="{ active: selectedApp === app }"
+            @click="selectApp(app)"
+            style="cursor: pointer;"
+        >
+          {{ index + 1 }}) {{ app }}
+        </li>
+      </ul>
+    </div>
+    <br>
+    <div v-if="appList != null && selectedApp != ''">
+      <h4>App Operations</h4>
+      <button id="button-edit-tags" @click="goToTagManagement()" class="btn btn-warning me-2">Edit Tags</button>
+      <!-- TODO There should be a confirmation dialog previously -->
+      <button id="button-delete-app" @click="deleteApp" class="btn btn-danger ms-2">Delete</button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent, onMounted, ref} from "vue";
+import router from "@/router";
+import {session} from "@/components/hub/shared";
+import axios from "axios";
+
+export default defineComponent({
+  name: 'HubAppManagement',
+
+  setup() {
+    const user = ref("");
+    const newAppToCreate = ref('');
+    const appList = ref<string[]>([]);
+    const selectedApp = ref<string>("");
+    const isEditingTags = ref<boolean>(false);
+
+    const selectApp = (app: string) => {
+      if (selectedApp.value == app) {
+        selectedApp.value = ""
+      } else {
+        selectedApp.value = app;
+      }
+    };
+
+    const goToTagManagement = () => {
+      router.push({ path: '/hub/tag-management', query: { user: user.value, app: selectedApp.value } });
+    }
+
+    const createApp = async () => {
+      const url = 'http://localhost:8082';
+      try {
+        await axios.post(url + '/apps', { value: newAppToCreate.value });
+      } catch (error) {
+        // TODO correctly interpret error, so that backend message is displayed.
+        alert("app creation error: " + error)
+      }
+      newAppToCreate.value = ""
+      await getApps()
+    };
+
+    const deleteApp = async () => {
+      const url = 'http://localhost:8082';
+      try {
+        await axios.delete(url + '/apps', {data: { value: selectedApp.value }});
+      } catch (error) {
+        // TODO correctly interpret error, so that backend message is displayed.
+        alert("app deletion error: " + error)
+      }
+      selectedApp.value = ""
+      await getApps()
+    };
+
+    const getApps = async () => {
+      const url = 'http://localhost:8082';
+      try {
+        const response = await axios.get(url + '/apps');
+        if (response.status === 200) {
+          appList.value = response.data as string[];
+          console.log("received apps: ", appList.value)
+        }
+      } catch (error) {
+        console.log("todo")
+      }
+    };
+
+    onMounted(() => {
+      user.value = session.user
+      getApps();
+    });
+
+    return {
+      isEditingTags,
+      appList,
+      selectedApp,
+      newAppToCreate,
+      selectApp,
+      goToTagManagement,
+      createApp,
+      deleteApp
+    }
+  },
+})
+</script>
--- src/cloud/frontend/src/components/hub/HubHome.vue
+++ src/cloud/frontend/src/components/hub/HubHome.vue
@@ -17,40 +17,7 @@
     </div>
     <br>
 
-    <div v-if="!isEditingTags">
-      <h4 >App Management</h4>
-      <div class="d-flex justify-content-center mb-3">
-        <div class="col-6">
-          <input id="input-app" v-model="newAppToCreate" class="form-control" placeholder="Name of New App" required />
-        </div>
-      </div>
-      <button id="button-create-app" @click="createApp" class="btn btn-primary">Create App</button>
-      <br>
-      <br>
-      <h4>App List</h4>
-      <p v-if="appList == null"> (no apps created yet) </p>
-      <div class="d-flex justify-content-center">
-        <ul id="app-list" class="list-group">
-          <li
-              v-for="(app, index) in appList"
-              :key="app"
-              class="list-group-item"
-              :class="{ active: selectedApp === app }"
-              @click="selectApp(app)"
-              style="cursor: pointer;"
-          >
-            {{ index + 1 }}) {{ app }}
-          </li>
-        </ul>
-      </div>
-      <br>
-      <div v-if="appList != null && selectedApp != ''">
-        <h4>App Operations</h4>
-        <button id="button-edit-tags" @click="goToTagManagement()" class="btn btn-warning me-2">Edit Tags</button>
-        <!-- TODO There should be a confirmation dialog previously -->
-        <button id="button-delete-app" @click="deleteApp" class="btn btn-danger ms-2">Delete</button>
-      </div>
-    </div>
+    <HubAppManagement></HubAppManagement>
 
     <div v-if="showDeleteConfirmation" class="modal fade show" style="display: block;" tabindex="-1">
       <div class="modal-dialog">
@@ -79,20 +46,18 @@ import { defineComponent, ref, onMounted } from 'vue';
 import axios from "axios";
 import router from "@/router";
 import {session} from "@/components/hub/shared";
+import HubAppManagement from "@/components/hub/HubAppManagement.vue";
 
 // TODO If app list is empty, then show an according message
 // TODO Select an app and then click on delete, easier to do
 // TODO Integrate error messages. Abstract the duplicate logic.
 export default defineComponent({
   name: 'HubComponent',
+  components: {HubAppManagement},
 
   setup() {
     const user = ref<string>("");
     const showDeleteConfirmation = ref(false);
-    const newAppToCreate = ref('');
-    const appList = ref<string[]>([]);
-    const selectedApp = ref<string>("");
-    const isEditingTags = ref<boolean>(false);
 
     // TODO App Management should be put to separate files/components and be imported here.
 
@@ -131,62 +96,12 @@ export default defineComponent({
       router.push('/hub/change-password');
     };
 
-    const createApp = async () => {
-      const url = 'http://localhost:8082';
-      try {
-        await axios.post(url + '/apps', { value: newAppToCreate.value });
-      } catch (error) {
-        // TODO correctly interpret error, so that backend message is displayed.
-        alert("app creation error: " + error)
-      }
-      newAppToCreate.value = ""
-      await getApps()
-    };
-
-    const deleteApp = async () => {
-      const url = 'http://localhost:8082';
-      try {
-        await axios.delete(url + '/apps', {data: { value: selectedApp.value }});
-      } catch (error) {
-        // TODO correctly interpret error, so that backend message is displayed.
-        alert("app deletion error: " + error)
-      }
-      selectedApp.value = ""
-      await getApps()
-    };
-
-    const getApps = async () => {
-      const url = 'http://localhost:8082';
-      try {
-        const response = await axios.get(url + '/apps');
-        if (response.status === 200) {
-          appList.value = response.data as string[];
-          console.log("received apps: ", appList.value)
-        }
-      } catch (error) {
-        console.log("todo")
-      }
-    };
-
     const visitCloud = () => {
       router.push('/');
     };
 
-    const selectApp = (app: string) => {
-      if (selectedApp.value == app) {
-        selectedApp.value = ""
-      } else {
-        selectedApp.value = app;
-      }
-    };
-
-    const goToTagManagement = () => {
-      router.push({ path: '/hub/tag-management', query: { user: user.value, app: selectedApp.value } });
-    }
-
     onMounted(() => {
       user.value = session.user
-      getApps();
     });
 
     return {
@@ -197,14 +112,6 @@ export default defineComponent({
       confirmDeleteAccount,
       redirectToChangePassword,
       visitCloud,
-      newAppToCreate,
-      createApp,
-      appList,
-      deleteApp,
-      isEditingTags,
-      selectedApp,
-      selectApp,
-      goToTagManagement,
     };
   },
 });
