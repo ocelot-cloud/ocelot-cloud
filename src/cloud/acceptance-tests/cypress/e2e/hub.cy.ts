00eba04d8fac02a6944c5502bae082aca4479585
Christoph Baier <chrisby@crispy-coding.org>
2024-08-22 09:22:31 +0200

    refactoring

--- src/cloud/acceptance-tests/cypress/e2e/hub.cy.ts
+++ src/cloud/acceptance-tests/cypress/e2e/hub.cy.ts
@@ -2,11 +2,11 @@ let authCookie = "";
 
 const hubWipePath = "http://localhost:8082/wipe-data"
 const cloudBaseUrl = "http://localhost:8081"
-const homePath = cloudBaseUrl + "/hub"
-const loginPath = cloudBaseUrl + "/hub/login"
-const registrationPath = cloudBaseUrl + "/hub/registration"
-const changePasswordPath = cloudBaseUrl + "/hub/change-password"
-const tagManagementPath = cloudBaseUrl + "/hub/tag-management"
+const homePath = `${cloudBaseUrl}/hub`
+const loginPath = `${cloudBaseUrl}/hub/login`
+const registrationPath = `${cloudBaseUrl}/hub/registration`
+const changePasswordPath = `${cloudBaseUrl}/hub/change-password`
+const tagManagementPath = `${cloudBaseUrl}/hub/tag-management`
 
 function createApp() {
     cy.get('#input-app').type('myapp');
@@ -64,13 +64,88 @@ function deleteApp() {
     cy.get('#button-delete-confirmation').should('not.exist');
 }
 
-function startToDeleteAppButCancel() {
+function tryToDeleteAppButCancelInConfirmationPopup() {
     cy.get('#app-list').find('li').click()
     cy.get('#button-delete-app').click();
     cy.get('#button-delete-cancel').click();
     cy.get('#button-delete-cancel').should('not.exist');
     cy.get('#button-delete-confirmation').should('not.exist');
     cy.get('#app-list').find('li').click()
+}
+
+function checkInputValidationOnLoginPage() {
+    cy.visit(loginPath);
+    cy.get('#input-username').type('ad');
+    cy.get('#input-password').type('pass');
+    cy.get('#input-username').should('not.have.class', 'is-invalid');
+    cy.get('#input-password').should('not.have.class', 'is-invalid');
+
+    cy.get('#button-login').click();
+    cy.get('#input-username').should('have.class', 'is-invalid');
+    cy.get('#input-password').should('have.class', 'is-invalid');
+    cy.get('body').should('contain.text', 'Invalid username,');
+    cy.get('body').should('contain.text', 'Invalid password,');
+
+    cy.get('#input-username').clear().type('admin');
+    cy.get('#input-password').clear().type('password');
+    cy.get('#input-username').should('not.have.class', 'is-invalid');
+    cy.get('#input-password').should('not.have.class', 'is-invalid');
+}
+
+function checkInputValidationOnRegistrationPage() {
+    cy.visit(registrationPath);
+    cy.get('#input-username').type('ad');
+    cy.get('#input-password').type('pass');
+    cy.get('#input-email').type('a@a');
+    cy.get('#input-username').should('not.have.class', 'is-invalid');
+    cy.get('#input-password').should('not.have.class', 'is-invalid');
+    cy.get('#input-email').should('not.have.class', 'is-invalid')
+
+    cy.get('#button-register').click();
+    cy.get('#input-username').should('have.class', 'is-invalid');
+    cy.get('#input-password').should('have.class', 'is-invalid');
+    cy.get('#input-email').should('have.class', 'is-invalid');
+    cy.get('body').should('contain.text', 'Invalid username,');
+    cy.get('body').should('contain.text', 'Invalid password,');
+    cy.get('body').should('contain.text', 'Invalid email,');
+
+    cy.get('#input-username').clear().type('admin');
+    cy.get('#input-password').clear().type('password');
+    cy.get('#input-email').clear().type('admin@admin.de');
+    cy.get('#input-username').should('not.have.class', 'is-invalid');
+    cy.get('#input-password').should('not.have.class', 'is-invalid');
+    cy.get('#input-email').should('not.have.class', 'is-invalid');
+}
+
+function checkInputValidationOnAppPage() {
+    login()
+    cy.get('#input-app').type('ad');
+    cy.get('#input-app').should('not.have.class', 'is-invalid');
+    cy.get('#button-create-app').click();
+    cy.get('#input-app').should('have.class', 'is-invalid');
+    cy.get('body').should('contain.text', 'Invalid app,');
+    cy.get('#input-app').clear().type('asdf');
+    cy.get('#input-app').should('not.have.class', 'is-invalid');
+}
+
+function checkInputValidationOnTagPage() {
+    cy.reload()
+    createApp()
+    clickOnApp()
+    cy.get('#button-edit-tags').click()
+    cy.get('input[type="file"]').selectFile({
+        contents: Cypress.Buffer.from(''),
+        fileName: 'as.tar.gz',
+    }, {force: true})
+    cy.contains('Invalid tag,').should('be.visible');
+    cy.get('input[type="file"]').selectFile({
+        contents: Cypress.Buffer.from(''),
+        fileName: 'asdf.tar.gz',
+    }, {force: true})
+    cy.should('not.contain', 'Invalid tag,');
+    cy.get('#tag-list').find('li').click()
+    cy.get('#button-delete-tag').click()
+    cy.get('#button-delete-confirmation').click()
 }
 
 describe('Hub Operations', () => {
@@ -97,78 +172,16 @@ describe('Hub Operations', () => {
         assertIsAppSelected(true)
         clickOnApp()
         assertIsAppSelected(false)
-        startToDeleteAppButCancel()
+        tryToDeleteAppButCancelInConfirmationPopup()
         deleteApp()
         assertEmptyAppList();
     });
 
     it('check input validation', () => {
-        cy.visit(loginPath);
-        cy.get('#input-username').type('ad');
-        cy.get('#input-password').type('pass');
-        cy.get('#input-username').should('not.have.class', 'is-invalid');
-        cy.get('#input-password').should('not.have.class', 'is-invalid');
-
-        cy.get('#button-login').click();
-        cy.get('#input-username').should('have.class', 'is-invalid');
-        cy.get('#input-password').should('have.class', 'is-invalid');
-        cy.get('body').should('contain.text', 'Invalid username,');
-        cy.get('body').should('contain.text', 'Invalid password,');
-
-        cy.get('#input-username').clear().type('admin');
-        cy.get('#input-password').clear().type('password');
-        cy.get('#input-username').should('not.have.class', 'is-invalid');
-        cy.get('#input-password').should('not.have.class', 'is-invalid');
-
-        cy.visit(registrationPath);
-        cy.get('#input-username').type('ad');
-        cy.get('#input-password').type('pass');
-        cy.get('#input-email').type('a@a');
-        cy.get('#input-username').should('not.have.class', 'is-invalid');
-        cy.get('#input-password').should('not.have.class', 'is-invalid');
-        cy.get('#input-email').should('not.have.class', 'is-invalid')
-
-        cy.get('#button-register').click();
-        cy.get('#input-username').should('have.class', 'is-invalid');
-        cy.get('#input-password').should('have.class', 'is-invalid');
-        cy.get('#input-email').should('have.class', 'is-invalid');
-        cy.get('body').should('contain.text', 'Invalid username,');
-        cy.get('body').should('contain.text', 'Invalid password,');
-        cy.get('body').should('contain.text', 'Invalid email,');
-
-        cy.get('#input-username').clear().type('admin');
-        cy.get('#input-password').clear().type('password');
-        cy.get('#input-email').clear().type('admin@admin.de');
-        cy.get('#input-username').should('not.have.class', 'is-invalid');
-        cy.get('#input-password').should('not.have.class', 'is-invalid');
-        cy.get('#input-email').should('not.have.class', 'is-invalid');
-
-        login()
-        cy.get('#input-app').type('ad');
-        cy.get('#input-app').should('not.have.class', 'is-invalid');
-        cy.get('#button-create-app').click();
-        cy.get('#input-app').should('have.class', 'is-invalid');
-        cy.get('body').should('contain.text', 'Invalid app,');
-        cy.get('#input-app').clear().type('asdf');
-        cy.get('#input-app').should('not.have.class', 'is-invalid');
-
-        cy.reload()
-        createApp()
-        clickOnApp()
-        cy.get('#button-edit-tags').click()
-        cy.get('input[type="file"]').selectFile({
-            contents: Cypress.Buffer.from(''),
-            fileName: 'as.tar.gz',
-        }, { force: true })
-        cy.contains('Invalid tag,').should('be.visible');
-        cy.get('input[type="file"]').selectFile({
-            contents: Cypress.Buffer.from(''),
-            fileName: 'asdf.tar.gz',
-        }, { force: true })
-        cy.should('not.contain', 'Invalid tag,');
-        cy.get('#tag-list').find('li').click()
-        cy.get('#button-delete-tag').click()
-        cy.get('#button-delete-confirmation').click()
+        checkInputValidationOnLoginPage();
+        checkInputValidationOnRegistrationPage();
+        checkInputValidationOnAppPage();
+        checkInputValidationOnTagPage();
     });
 
     it('check upload', () => {
