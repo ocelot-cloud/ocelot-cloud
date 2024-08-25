3caed2b8dda8e1c2ae28b954582fec2b31b641e5
Christoph Baier <chrisby@crispy-coding.org>
2024-08-25 13:05:19 +0200

    refactoring

--- scripts/build.sh
+++ scripts/build.sh
@@ -22,17 +22,21 @@ rsync -a --delete "$BACKEND_DIR" "$ARTIFACTS_DIR/"
 echo "Building components"
 cd "$DOCKER_DIR"
 
+# TODO Resolve duplication of the docker images, there are two times here and once in the Dockerfiles each
 echo "1. Building frontend"
+if [ -z "$(docker images -q node:18.10.0)" ]; then docker pull node:18.10.0; fi
 docker build -f Dockerfile.frontend -t frontend-builder:local .
 docker run --name frontend-builder -v frontend-deps:/app/node_modules frontend-builder:local
 docker cp frontend-builder:/app/dist "$FRONTEND_ARTIFACTS_DIR"
 docker rm -f frontend-builder
 
 echo "2. Building backend"
+if [ -z "$(docker images -q golang:1.21.8)" ]; then docker pull golang:1.21.8; fi
 docker build -f Dockerfile.backend -t backend-builder:local .
 docker run --name backend-builder -v backend-deps:/go/pkg/mod backend-builder:local .
 docker cp backend-builder:/go/src/app/backend "$BACKEND_ARTIFACTS_DIR"
 docker rm -f backend-builder
 
 echo "Building production image"
+if [ -z "$(docker images -q alpine:3.18.6)" ]; then docker pull alpine:3.18.6; fi
 docker build -t ocelotcloud/ocelotcloud:local -f Dockerfile.production .
