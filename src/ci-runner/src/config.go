675a71d4f08b6fc9d1af05f42568df2e65c94f86
Christoph Baier <chrisby@crispy-coding.org>
2024-08-27 08:27:04 +0200

    refactoring

---
+++ src/ci-runner/src/config.go
@@ -0,0 +1,54 @@
+package src
+
+import (
+	"os"
+	"path/filepath"
+)
+
+const ocelotContainerRunCommand = "docker compose -p ocelot-cloud up"
+const ocelotContainerRunCommandDetached = "docker compose -p ocelot-cloud up -d"
+const cypressCommand = "npx cypress run --spec cypress/e2e/cloud.cy.ts --headless"
+
+var projectDir = GetProjectDir()
+var scriptsDir = projectDir + "/scripts"
+var srcDir = projectDir + "/src"
+var cloudDir = srcDir + "/cloud"
+var backendDir = cloudDir + "/backend"
+var backendComponentTestsDir = backendDir + "/component-tests"
+var frontendDir = cloudDir + "/frontend"
+var acceptanceTestsDir = cloudDir + "/acceptance-tests"
+var ocelotStackDir = backendDir + "/stacks/core/ocelot-cloud"
+var backendBusinessInternalDir = backendDir + "/business/internal"
+var backendSecurityInternalDir = backendDir + "/security/internal"
+var hubDir = srcDir + "/hub"
+
+// TestProfile There is also the "PROD" profile, but it should be used automatically if no profile is given.
+const TestProfile = "TEST"
+
+func GetProjectDir() string {
+	devopsRunnerDir, _ := os.Getwd()
+	src := filepath.Dir(devopsRunnerDir)
+	return filepath.Dir(src)
+}
+
+func getEnableMocksEnv(enabled bool) string {
+	prefix := "ENABLE_MOCKS="
+	if enabled {
+		return prefix + "true"
+	} else {
+		return prefix + "false"
+	}
+}
+
+func getTestProfileEnv() string {
+	return "PROFILE=" + TestProfile
+}
+
+func getEnableDummyStacksEnv(enabled bool) string {
+	prefix := "USE_DUMMY_STACKS="
+	if enabled {
+		return prefix + "true"
+	} else {
+		return prefix + "false"
+	}
+}
--- src/ci-runner/src/tests_cloud.go
+++ src/ci-runner/src/tests_cloud.go
@@ -1,36 +1,8 @@
 package src
 
 import (
-	"os"
 	"os/exec"
-	"path/filepath"
 )
-
-const ocelotContainerRunCommand = "docker compose -p ocelot-cloud up"
-const ocelotContainerRunCommandDetached = "docker compose -p ocelot-cloud up -d"
-const cypressCommand = "npx cypress run --spec cypress/e2e/cloud.cy.ts --headless"
-
-var projectDir = GetProjectDir()
-var scriptsDir = projectDir + "/scripts"
-var srcDir = projectDir + "/src"
-var cloudDir = srcDir + "/cloud"
-var backendDir = cloudDir + "/backend"
-var backendComponentTestsDir = backendDir + "/component-tests"
-var frontendDir = cloudDir + "/frontend"
-var acceptanceTestsDir = cloudDir + "/acceptance-tests"
-var ocelotStackDir = backendDir + "/stacks/core/ocelot-cloud"
-var backendBusinessInternalDir = backendDir + "/business/internal"
-var backendSecurityInternalDir = backendDir + "/security/internal"
-var hubDir = srcDir + "/hub"
-
-// TestProfile There is also the "PROD" profile, but it should be used automatically if no profile is given.
-const TestProfile = "TEST"
-
-func GetProjectDir() string {
-	devopsRunnerDir, _ := os.Getwd()
-	src := filepath.Dir(devopsRunnerDir)
-	return filepath.Dir(src)
-}
 
 func testBackendCore() {
 	printTestDescription("Testing backend testing API")
@@ -67,19 +39,6 @@ func TestBackendComponentMocked() {
 	ExecuteInDir(backendComponentTestsDir, "go test -v -count=1 component_test.go")
 }
 
-func getTestProfileEnv() string {
-	return "PROFILE=" + TestProfile
-}
-
-func getEnableDummyStacksEnv(enabled bool) string {
-	prefix := "USE_DUMMY_STACKS="
-	if enabled {
-		return prefix + "true"
-	} else {
-		return prefix + "false"
-	}
-}
-
 func TestCloudAcceptance() {
 	printTestDescription("Testing acceptance")
 	defer Cleanup()
@@ -89,15 +48,6 @@ func TestCloudAcceptance() {
 	WaitForIndexPageToBeReady(ocelotUrl)
 	Build(Acceptance)
 	ExecuteInDir(acceptanceTestsDir, cypressCommand)
-}
-
-func getEnableMocksEnv(enabled bool) string {
-	prefix := "ENABLE_MOCKS="
-	if enabled {
-		return prefix + "true"
-	} else {
-		return prefix + "false"
-	}
 }
 
 func DeployLocally() {
