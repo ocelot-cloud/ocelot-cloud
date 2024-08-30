6539b5470501bed3434927d57c69ce7431978453
Christoph Baier <chrisby@crispy-coding.org>
2024-08-30 14:11:28 +0200

    refactoring

--- src/cloud/backend/security/repository.go
+++ src/cloud/backend/security/repository.go
@@ -32,71 +32,71 @@ func initializeDatabaseWithSource(dataSourceName string) {
 // TODO Initial design for the data model, during implementation/testing you should check if correct.
 func initializeTables() {
 	_, err := db.Exec(`
-	CREATE TABLE IF NOT EXISTS users (
-		user_id SERIAL PRIMARY KEY,
-		user_name VARCHAR(255) NOT NULL UNIQUE,
-		hashed_password VARCHAR(255) NOT NULL,
-		hashed_cookie VARCHAR(255),
-		is_admin BOOLEAN NOT NULL
-	);
-`)
+		CREATE TABLE IF NOT EXISTS users (
+			user_id SERIAL PRIMARY KEY,
+			user_name VARCHAR(255) NOT NULL UNIQUE,
+			hashed_password VARCHAR(255) NOT NULL,
+			hashed_cookie VARCHAR(255),
+			is_admin BOOLEAN NOT NULL
+		);
+	`)
 	if err != nil {
 		Logger.Fatal("Failed to create users table: %v", err)
 	}
 
 	_, err = db.Exec(`
-	CREATE TABLE IF NOT EXISTS apps (
-		app_id SERIAL PRIMARY KEY,
-		maintainer_name VARCHAR(255) NOT NULL,
-		app_name VARCHAR(255) NOT NULL,
-		UNIQUE (maintainer_name, app_name)
-	);
-`)
+		CREATE TABLE IF NOT EXISTS apps (
+			app_id SERIAL PRIMARY KEY,
+			maintainer_name VARCHAR(255) NOT NULL,
+			app_name VARCHAR(255) NOT NULL,
+			UNIQUE (maintainer_name, app_name)
+		);
+	`)
 	if err != nil {
 		Logger.Fatal("Failed to create apps table: %v", err)
 	}
 
 	_, err = db.Exec(`
-	CREATE TABLE IF NOT EXISTS tags (
-		app_id INT REFERENCES apps(app_id) ON DELETE CASCADE,
-		tag_id SERIAL PRIMARY KEY,
-		tag_name VARCHAR(255) NOT NULL,
-		content_blob BYTEA NOT NULL,
-		UNIQUE (app_id, tag_name)
-	);
-`)
+		CREATE TABLE IF NOT EXISTS tags (
+			app_id INT REFERENCES apps(app_id) ON DELETE CASCADE,
+			tag_id SERIAL PRIMARY KEY,
+			tag_name VARCHAR(255) NOT NULL,
+			content_blob BYTEA NOT NULL,
+			UNIQUE (app_id, tag_name)
+		);
+	`)
 	if err != nil {
 		Logger.Fatal("Failed to create tags table: %v", err)
 	}
 
 	_, err = db.Exec(`
-	CREATE TABLE IF NOT EXISTS groups (
-		group_id SERIAL PRIMARY KEY,
-		group_name VARCHAR(255) NOT NULL UNIQUE 
-	);
-`)
+		CREATE TABLE IF NOT EXISTS groups (
+			group_id SERIAL PRIMARY KEY,
+			group_name VARCHAR(255) NOT NULL UNIQUE 
+		);
+	`)
 	if err != nil {
 		Logger.Fatal("Failed to create groups table: %v", err)
 	}
 
 	_, err = db.Exec(`
-	CREATE TABLE IF NOT EXISTS user_to_group (
-		user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
-		group_id INT REFERENCES groups(group_id) ON DELETE CASCADE,
-		PRIMARY KEY (user_id, group_id)
-	);
-`)
+		CREATE TABLE IF NOT EXISTS user_to_group (
+			user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
+			group_id INT REFERENCES groups(group_id) ON DELETE CASCADE,
+			PRIMARY KEY (user_id, group_id)
+		);
+	`)
 	if err != nil {
 		Logger.Fatal("Failed to create user_to_group table: %v", err)
 	}
 
 	_, err = db.Exec(`
-	CREATE TABLE IF NOT EXISTS app_access (
-		group_id INT REFERENCES groups(group_id) ON DELETE CASCADE,
-		app_id INT REFERENCES apps(app_id) ON DELETE CASCADE,
-		PRIMARY KEY (group_id, app_id)
-	);
-`)
+		CREATE TABLE IF NOT EXISTS app_access (
+			group_id INT REFERENCES groups(group_id) ON DELETE CASCADE,
+			app_id INT REFERENCES apps(app_id) ON DELETE CASCADE,
+			PRIMARY KEY (group_id, app_id)
+		);
+	`)
 	if err != nil {
 		Logger.Fatal("Failed to create app_access table: %v", err)
 	}
