9676a0d118c4031bbecde3f9c1eb3af025556b4f
Christoph Baier <chrisby@crispy-coding.org>
2024-09-05 12:37:02 +0200

    refactoring

--- src/cloud/backend/setup/database.go
+++ src/cloud/backend/setup/database.go
@@ -35,16 +35,20 @@ func createAdminUserIfNotExistent(adminNameEnv string, adminPasswordEnv string) 
 		return nil
 	} else {
 		logger.Info("Application needs at least one admin user, but none was found in database. Trying to create the admin user from env variables.")
-		if adminNameEnv == "" {
-			return fmt.Errorf("necessary env variable '%s' is not set", initialAdminNameEnv)
-		} else if adminPasswordEnv == "" {
-			return fmt.Errorf("necessary env variable '%s' is not set", initialAdminPasswordEnv)
-		} else {
-			err := repo.CreateUser(adminPasswordEnv, adminPasswordEnv, true)
-			if err != nil {
-				return fmt.Errorf("initial admin user creation from env variables failed: %v", err)
-			}
-			return nil
+		return createAdminsUserFromEnvs(adminNameEnv, adminPasswordEnv)
+	}
+}
+
+func createAdminsUserFromEnvs(adminNameEnv string, adminPasswordEnv string) error {
+	if adminNameEnv == "" {
+		return fmt.Errorf("necessary env variable '%s' is not set", initialAdminNameEnv)
+	} else if adminPasswordEnv == "" {
+		return fmt.Errorf("necessary env variable '%s' is not set", initialAdminPasswordEnv)
+	} else {
+		err := repo.CreateUser(adminPasswordEnv, adminPasswordEnv, true)
+		if err != nil {
+			return fmt.Errorf("initial admin user creation from env variables failed: %v", err)
 		}
+		return nil
 	}
 }
