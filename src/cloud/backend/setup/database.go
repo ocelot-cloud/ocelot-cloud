4ff4f5d1d3af54ccf26af8aeeb2612fe36170b28
Christoph Baier <chrisby@crispy-coding.org>
2024-09-05 11:22:47 +0200

    refactoring

--- src/cloud/backend/setup/database.go
+++ src/cloud/backend/setup/database.go
@@ -3,6 +3,7 @@ package setup
 import (
 	"ocelot/backend/security"
 	"ocelot/backend/tools"
+	"os"
 )
 
 // TODO Maybe put that stuff in the security module? Also this isn't just security, but also other stuff. Maybe create a "repository" package?
@@ -13,14 +14,12 @@ func InitializeDatabase(config *tools.GlobalConfig) {
 		security.InitializeDatabaseWithSource(":memory:")
 	}
 
-	err := createAdminUserIfNotExistent()
+	err := createAdminUserIfNotExistent(os.Getenv("INITIAL_ADMIN_NAME"), os.Getenv("INITIAL_ADMIN_PASSWORD"))
 	if err != nil {
 		logger.Fatal("Admin user initialization failed: %v", err)
 	}
 }
 
-func createAdminUserIfNotExistent() error {
-	// TODO Check if admin user exists. If not take it from the env variables. If not existent, crash.
-	// TODO Add tests: 1) neither admin in repo nor in envs -> crash, 2) no admin in repo, but in envs -> no crash, 3) admin in repo, but not in envs -> no crash
+func createAdminUserIfNotExistent(adminNameEnv string, adminPasswordEnv string) error {
 	return nil
 }
--- src/cloud/backend/setup/database_test.go
+++ src/cloud/backend/setup/database_test.go
@@ -3,5 +3,8 @@ package setup
 import "testing"
 
 func TestAsdf(t *testing.T) {
-	// TODO
+	// TODO Check if admin user exists. If not take it from the env variables. If not existent, crash.
+	// TODO Add tests: 1) neither admin in repo nor in envs -> crash, 2) no admin in repo, but in envs -> no crash, 3) admin in repo, but not in envs -> no crash
+
+	// TODO createAdminUserIfNotExistent()
 }
