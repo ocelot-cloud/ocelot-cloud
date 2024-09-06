a38399d885a86323bf9b5a7b5270f8c667df38c5
Christoph Baier <chrisby@crispy-coding.org>
2024-09-06 14:15:28 +0200

    refactoring

--- src/cloud/frontend/src/components/cloud/Config.ts
+++ src/cloud/frontend/src/components/cloud/Config.ts
@@ -1,26 +1,19 @@
-import {BackendClient} from "@/components/cloud/BackendClient";
-import axios, {AxiosResponse} from "axios";
-
 export const scheme = "http"
 export const baseDomain = "localhost"
 
 interface GlobalConfig {
     cloudBaseUrl: string;
     stackUrl: string;
-    backendClient: BackendClient;
 }
 
 function getGlobalConfig(): GlobalConfig {
     const PROFILE = import.meta.env.VITE_APP_PROFILE || PROFILE_VALUES.PROD;
     let cloudBaseUrl: string;
-    let backendClient: BackendClient;
 
     if (PROFILE === PROFILE_VALUES.TEST) {
         cloudBaseUrl = 'http://localhost:8080';
-        backendClient = new BackendClient();
     } else if (PROFILE === PROFILE_VALUES.PROD) {
         cloudBaseUrl = 'http://ocelot-cloud.' + 'localhost';
-        backendClient = new BackendClient();
     } else {
         throw new Error("error, provided VITE_APP_PROFILE is not known: " + PROFILE);
     }
@@ -30,7 +23,6 @@ function getGlobalConfig(): GlobalConfig {
     return {
         cloudBaseUrl,
         stackUrl,
-        backendClient
     };
 }
 
