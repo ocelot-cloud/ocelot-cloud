13626dc663df10769cf5ae18a24193764d3fcf7e
Christoph Baier <chrisby@crispy-coding.org>
2024-09-06 15:31:02 +0200

    refactoring

--- src/cloud/backend/component-tests/user_management_test.go
+++ src/cloud/backend/component-tests/user_management_test.go
@@ -1,8 +1,33 @@
 package component_tests
 
-import "testing"
+import (
+	"github.com/ocelot-cloud/shared/utils"
+	"testing"
+)
+
+type CloudClient struct {
+	parent utils.ComponentClient
+	stack  string
+}
 
 // TODO user registration, login?, authorization and authentication etc
 func TestAsd(t *testing.T) {
+	client := getDefaultCloudClient()
+	println(client.stack)
+}
 
+func getDefaultCloudClient() CloudClient {
+	return CloudClient{
+		utils.ComponentClient{
+			"admin",
+			"password",
+			"password" + "x",
+			"http://localhost:8081",
+			nil,
+			true,
+			true,
+			"http://localhost:8081",
+		},
+		"nginx-default",
+	}
 }
