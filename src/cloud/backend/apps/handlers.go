cd4f5eccac0ecb0a18c5dc7a1918d60a160e856a
Christoph Baier <chrisby@crispy-coding.org>
2024-09-07 09:59:06 +0200

    refactoring

---
+++ src/cloud/backend/apps/handlers.go
@@ -0,0 +1,91 @@
+package apps
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"ocelot/backend/security"
+	"ocelot/backend/tools"
+)
+
+func registerSecuredEndpoint(path string, handlerFunc http.HandlerFunc) {
+	router.Handle("/api"+path, security.ApplyAuthMiddleware(handlerFunc))
+}
+
+func createReadHandler(stackService appServiceType) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "GET" {
+			http.Error(w, "Only GET method is supported.", http.StatusMethodNotAllowed)
+			return
+		}
+
+		stackStateInfo := stackService.getAppStateInfo()
+		response := make([]tools.ResponsePayloadDto, 0)
+		for stackName, stackDetails := range stackStateInfo {
+			response = append(response, tools.ResponsePayloadDto{stackName, stackDetails.State.toString(), stackDetails.Path})
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response)
+	}
+}
+
+func createDeployHandler(stackService appServiceType) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "POST" {
+			http.Error(w, "Only POST method is supported.", http.StatusMethodNotAllowed)
+			return
+		}
+
+		stackName, err := decodeStackInfo(r)
+		if err != nil {
+			http.Error(w, "Failed to decode JSON", http.StatusBadRequest)
+			return
+		}
+
+		if err = stackService.deployApp(stackName); err != nil {
+			if err != nil {
+				logger.Error("Deploying stack failed: " + stackName + "\n" + err.Error() + "\n")
+				http.Error(w, "Deploying stack failed: "+stackName, http.StatusInternalServerError)
+			}
+			return
+		}
+	}
+}
+
+func createStopHandler(stackService appServiceType) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "POST" {
+			http.Error(w, "Only POST method is supported.", http.StatusMethodNotAllowed)
+			return
+		}
+
+		stackName, err := decodeStackInfo(r)
+		if err != nil {
+			http.Error(w, "Failed to decode JSON", http.StatusBadRequest)
+			return
+		}
+
+		if err = stackService.stopApp(stackName); err != nil {
+			if err != nil {
+				logger.Warn("error when trying to stop stack, %s", err.Error())
+				http.Error(w, "Stopping stack failed: "+stackName, http.StatusInternalServerError)
+			}
+			return
+		}
+	}
+}
+
+func decodeStackInfo(r *http.Request) (string, error) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		return "", err
+	}
+
+	var stackInfo tools.StackInfo
+	err = json.Unmarshal(body, &stackInfo)
+	if err != nil {
+		return "", err
+	}
+	return stackInfo.Name, nil
+}
