420f3f59e71ed57ebd25c4bbaf0f5d88f30f11bc
Christoph Baier <chrisby@crispy-coding.org>
2024-09-14 09:25:10 +0200

    add user to group

--- src/cloud/backend/security/repository_groups.go
+++ src/cloud/backend/security/repository_groups.go
@@ -1,5 +1,10 @@
 package security
 
+import (
+	"fmt"
+	"strings"
+)
+
 func (r *MyRepository) CreateGroup(group string) error {
 	_, err := DB.Exec("INSERT INTO groups(group_name) VALUES (?)", group)
 	if err != nil {
@@ -55,14 +60,104 @@ func (r *MyRepository) ListAllUsers() ([]string, error) {
 	return users, nil
 }
 
+// TODO deletion cascading: when either the group or the user is deleted, this entry should be deleted as well.
 func (r *MyRepository) AddUserToGroup(user, group string) error {
-	//TODO implement me
-	panic("implement me")
+	userId, err := r.getUserId(user)
+	if err != nil {
+		// TODO
+		return err
+	}
+	groupId, err := r.getGroupId(group)
+	if err != nil {
+		// TODO
+		return err
+	}
+
+	_, err = DB.Exec("INSERT INTO user_to_group(user_id, group_id) VALUES (?, ?)", userId, groupId)
+	if err != nil {
+		// TODO
+		return err
+	}
+	return nil
+}
+
+func (r *MyRepository) getUserId(user string) (int, error) {
+	var userId int
+	err := DB.QueryRow("Select user_id from users where user_name = ?", user).Scan(&userId)
+	if err != nil {
+		// TODO
+		return -1, err
+	}
+	return userId, nil
+}
+
+func (r *MyRepository) getGroupId(group string) (int, error) {
+	var groupId int
+	err := DB.QueryRow("Select group_id from groups where group_name = ?", group).Scan(&groupId)
+	if err != nil {
+		// TODO
+		return -1, err
+	}
+	return groupId, nil
 }
 
 func (r *MyRepository) ListMembersOfGroup(group string) ([]string, error) {
-	//TODO implement me
-	panic("implement me")
+	groupId, err := r.getGroupId(group)
+	if err != nil {
+		// TODO
+		return nil, err
+	}
+	rows, err := DB.Query("SELECT user_id FROM user_to_group WHERE group_id = ?", groupId)
+	if err != nil {
+		// TODO
+		return nil, err
+	}
+	defer rows.Close()
+	var userIds []int
+	for rows.Next() {
+		var userId int
+		if err = rows.Scan(&userId); err != nil {
+			// TODO
+			return nil, err
+		}
+		userIds = append(userIds, userId)
+	}
+
+	usernames, err := r.getUsernamesByIDs(userIds)
+	if err != nil {
+		// TODO
+		return nil, err
+	}
+
+	return usernames, nil
+}
+
+func (r *MyRepository) getUsernamesByIDs(ids []int) ([]string, error) {
+	query := fmt.Sprintf("SELECT user_name FROM users WHERE user_id IN (%s)", strings.TrimSuffix(strings.Repeat("?,", len(ids)), ","))
+
+	args := make([]interface{}, len(ids))
+	for i, id := range ids {
+		args[i] = id
+	}
+
+	rows, err := DB.Query(query, args...)
+	if err != nil {
+		// TODO
+		return nil, err
+	}
+	defer rows.Close()
+
+	var usernames []string
+	for rows.Next() {
+		var username string
+		if err = rows.Scan(&username); err != nil {
+			// TODO
+			return nil, err
+		}
+		usernames = append(usernames, username)
+	}
+
+	return usernames, nil
 }
 
 func (r *MyRepository) RemoveUserFromGroup(user, group string) error {
--- src/cloud/backend/security/repository_groups_test.go
+++ src/cloud/backend/security/repository_groups_test.go
@@ -37,3 +37,18 @@ func TestListAllUsers(t *testing.T) {
 	assert.Equal(t, 1, len(users))
 	assert.Equal(t, sampleUser, users[0])
 }
+
+func TestAddUserToGroup(t *testing.T) {
+	assert.Nil(t, repo.CreateUser(sampleUser, samplePassword, true))
+	assert.Nil(t, repo.CreateGroup(sampleGroup))
+
+	members, err := repo.ListMembersOfGroup(sampleGroup)
+	assert.Nil(t, err)
+	assert.Equal(t, 0, len(members))
+	assert.Nil(t, repo.AddUserToGroup(sampleUser, sampleGroup))
+
+	members, err = repo.ListMembersOfGroup(sampleGroup)
+	assert.Nil(t, err)
+	assert.Equal(t, 1, len(members))
+	assert.Equal(t, sampleUser, members[0])
+}
