213ef2f9c0d295b6dd7229de90c019d59978a2e3
Christoph Baier <chrisby@crispy-coding.org>
2024-10-10 13:39:33 +0200

    Revert "refactoring"
    This reverts commit f25766dd709d2efce72a08744a77829cfffd58dc.

--- src/cloud/backend/apps_new/hub_client_test.go
+++ src/cloud/backend/apps_new/hub_client_test.go
@@ -1,14 +1,7 @@
 package apps_new
 
-import (
-	"testing"
-)
+import "testing"
 
-// TODO All the hub client data are made for tests. I think I need a createHubClient function in which I set all params for production use.
-// TODO Implement and test via client: search apps, download app
-// TODO Create ci-runner test: 1) run hub, then 2) run cloud client tests
-// TODO Create a mock for this client: 1) hubClientReal and 2) hubClientMock, also add ENV in order to switch between them
 func TestRegister(t *testing.T) {
-	/*client := hub.GetHub()
-	client.RegisterUser()*/
+	// TODO
 }
