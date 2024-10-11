89227d590cc2cb8e75e87d888de80ae0f5c210b8
Christoph Baier <chrisby@crispy-coding.org>
2024-10-11 14:20:24 +0200

    refactoring

--- src/cloud/backend/apps_new/hubHandler.go
+++ src/cloud/backend/apps_new/hubHandler.go
@@ -1,1 +1,22 @@
 package apps_new
+
+import "net/http"
+
+// TODO There are three kind of endpoints that must be distinguished: public (like login), user-level (like readApps), admin-level (like start/stop apps) -> should be specific functions for registration.
+//   registerPublicEndpoint("path", handler), registerUserEndpoint(...), registerAdminEndpoint(...)
+
+// TODO Re-use the approach to read dto's from requests like it was done in
+func AppDownloadHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func AppStartHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func AppStopHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// TODO readAppHandler, home page -> users can only see available apps and open them, no start or stop visible or allowed by backend.
+//   Home page must distinguish between users and admins.
