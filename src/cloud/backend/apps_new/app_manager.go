1db6a92f1960b2fcfdf689fd30b573c87613cdca
Christoph Baier <chrisby@crispy-coding.org>
2024-10-11 13:41:08 +0200

    refactoring

--- src/cloud/backend/apps_new/app_manager.go
+++ src/cloud/backend/apps_new/app_manager.go
@@ -13,11 +13,9 @@ import (
 	"strings"
 )
 
-// TODO Add a test/function which takes the zip bytes, extracts them locally and run "docker-compose up" on them.
-// TODO in shared.utils remove the "storage" stuff: Logger.Info("zipped directory %s and stored its %v bytes in the database for integration testing", dirPath, len(buf.Bytes()))
 // TODO Add logs to errors.
 
-var hubClient HubClient // TODO should be set by the application and the tests depending on the current profile
+var hubClient HubClient
 
 func DownloadTag(info TagInfo) error {
 	tagContent, err := hubClient.DownloadTag(info)
